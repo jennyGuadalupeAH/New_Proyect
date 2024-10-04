af8bd8fd2006a7e0e63dd82fd4fe23599fbe7d5a
Jennifer Guadalupe <jennyangeleshernandez310@gmail.com>
2024-10-04 15:41:34 -0600

    ADD: uso de estructura de control IF AND WHEN

--- app/src/main/java/com/tesji/hello_world_kotlin/MainActivity.kt
+++ app/src/main/java/com/tesji/hello_world_kotlin/MainActivity.kt
@@ -28,7 +28,34 @@ class MainActivity : AppCompatActivity() {
             var edad=17
             println("Edad=" + edad)
         }
-        edad = edad + 1
+        //operaciones logicos
+        if (edad >=18){
+            println("Eres menor de edad")
+        }else if(edad >18 && edad<=40){
+            println("Eres joven")
+        }else if(edad > 40 && edad <=60){
+            println("Ya eres adulto")
+        }else{
+            println("Eres adulto mayor")
+        }
+        var numMes:Int = 6
+
+        var result = when(numMes){
+            1->"Enero"
+            2->"Febrero"
+            3->"Marzo"
+            4->"Abril"
+            5->"Mayo"
+            6->"Junio"
+            7->"Julio"
+            8->"Agosto"
+            9->"Septiembre"
+            10->"Octubre"
+            11->"Noviembre"
+            12->"Diciembre"
+            else -> "NO COINCIDE EL MES CON NUMERO"
+        }
+        println("Mes de: " + result)
 
 
 
